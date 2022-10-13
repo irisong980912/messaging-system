8df3c1c416f9de9ea6aeb5c9f23d1ab7f09485f1
Jianjin Fan <jianjinf@amazon.com>
2022-10-13 18:06:25 -0400

    Set up database for CI.

---
+++ init.sql
@@ -0,0 +1,25 @@
+USE messaging;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `nickname` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `password` varchar(128) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `login_token` varchar(128) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `register_time` datetime DEFAULT NULL,
+  `last_login_time` datetime DEFAULT NULL,
+  `gender` varchar(128) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `address` varchar(128) DEFAULT NULL,
+  `is_valid` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_uniq_index` (`username`),
+  UNIQUE KEY `email_uniq_index` (`email`),
+  KEY `username_index` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `user_validation_code` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `validation_code` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
